ce25b262275141ffe269ed3f2cedf333425bd97d
user <user@user>
2021-06-06 22:10:51 +0200

    With PC

--- ListModules/Source.cpp
+++ ListModules/Source.cpp
@@ -1,9 +1,11 @@
 ﻿#pragma comment(lib, "aux_klib.lib")
 #include <ntddk.h>
 #include <aux_klib.h>
+#include <intrin.h> // __readmsr
 #include "nt.h"
 
 // https://github.com/vxcute/WindowsInternals/blob/main/Misc/Snippets/KernelMode/KmGetNtosImageBase.cpp
+// https://m0uk4.gitbook.io/notebooks/mouka/windowsinternal/find-kernel-module-address-todo
 
 EXTERN_C_START
 DRIVER_INITIALIZE DriverEntry;
@@ -15,6 +17,9 @@ EXTERN_C_END
 #endif
 
 constexpr ULONG TAG = 'kedr';
+
+// Target RIP for the called procedure when SYSCALL is executed in 64-bit mode.
+constexpr auto IA32_LSTAR = 0xC0000082;
 
 template <class T>
 auto GetRoutineAddress(_In_ PUNICODE_STRING routineName) -> T
@@ -31,7 +36,7 @@ auto GetRoutineAddress(_In_ PUNICODE_STRING routineName) -> T
 
 
 typedef NTSTATUS (*_ZwQuerySystemInformation)(ULONG SystemInformationClass, PVOID SystemInformation, ULONG SystemInformationLength, PULONG ReturnLength);
-NTSTATUS ListModulesWithQuerySystemInformation(VOID)
+NTSTATUS GetNtOsKrnlWithQuerySystemInformation(VOID)
 {
     auto status = STATUS_SUCCESS;
     __try {
@@ -39,7 +44,7 @@ NTSTATUS ListModulesWithQuerySystemInformation(VOID)
         ULONG modulesSize = 0;
         PSYSTEM_MODULE_INFORMATION modules = nullptr;
         auto SystemModuleInformation = 0xb;
-        UNICODE_STRING routineName = RTL_CONSTANT_STRING(L"ZwQuerySystemInformation");
+        UNICODE_STRING routineName = RTL_CONSTANT_STRING(L"ZwQuerySystemInformation"); // gone from win8 orly?
 
         // auto NtQuerySystemInformation = (_NtQuerySystemInformation)GetProcAddress(ntdll, "NtQuerySystemInformation");
         auto ZwQuerySystemInformation = GetRoutineAddress<_ZwQuerySystemInformation>(&routineName);
@@ -62,7 +67,6 @@ NTSTATUS ListModulesWithQuerySystemInformation(VOID)
         if (!NT_SUCCESS(status))
             goto Exit;
 
-        DbgPrint("modules:");
         for (ULONG i = 0; i < modules->ModulesCount; ++i)
         {
             auto module = modules->Modules[i];
@@ -81,7 +85,7 @@ NTSTATUS ListModulesWithQuerySystemInformation(VOID)
     __except (1) {}
 }
 
-NTSTATUS ListModulesWithAuxKlib(VOID)
+NTSTATUS GetNtOsKrnlWithAuxKlib(VOID)
 {
     ULONG modulesSize = 0;
     ULONG moduleSize = sizeof(AUX_MODULE_EXTENDED_INFO);
@@ -116,7 +120,6 @@ NTSTATUS ListModulesWithAuxKlib(VOID)
         goto Exit;
     }
 
-    DbgPrint("modules:\n");
     for (ULONG i = 0; i < modulesSize / moduleSize; ++i)
     {
         auto module = modules[i];
@@ -138,6 +141,75 @@ Exit:
     return status;
 }
 
+typedef PVOID (*_RtlLookupFunctionEntry)(DWORD64 ControlPc, PDWORD64 ImageBase, PVOID /* PUNWIND_HISTORY_TABLE */ HistoryTable);
+PVOID GetNtOsKrnlWithPC(VOID)
+{
+    __try {
+        UNICODE_STRING RoutineName = RTL_CONSTANT_STRING(L"RtlLookupFunctionEntry");
+        auto RtlLookupFunctionEntry = GetRoutineAddress<_RtlLookupFunctionEntry>(&RoutineName);
+        DWORD64 NtImageBase = 0;
+        RtlLookupFunctionEntry( // Searches the active function tables for an entry that corresponds to the specified PC value.
+            (DWORD64)&MmFreeContiguousMemorySpecifyCache,
+            &NtImageBase,
+            nullptr
+        );
+        if (NtImageBase == 0)
+        {
+            DbgPrint("Cannot get base address\n");
+            return nullptr;
+        }
+
+        DbgPrint("ntoskrnl.exe at 0x%p\n", NtImageBase);
+        return (PVOID)NtImageBase;
+
+    }
+    __except (1) {}
+}
+
+typedef PVOID(*_RtlPcToFileHeader)(PVOID PcValue, PVOID* BasoOfImage);
+PVOID GetNtOsKrnlWithPC2(VOID)
+{
+    __try {
+        UNICODE_STRING RoutineName = RTL_CONSTANT_STRING(L"RtlPcToFileHeader");
+        auto RtlPcToFileHeader = GetRoutineAddress<_RtlPcToFileHeader>(&RoutineName);
+        PVOID NtImageBase = nullptr;
+        RtlPcToFileHeader( // Retrieve the base address of the image that contains the specified PC value.
+            &MmFreeContiguousMemorySpecifyCache,
+            &NtImageBase
+        );
+        if (NtImageBase == nullptr)
+        {
+            DbgPrint("Cannot get base address\n");
+            return nullptr;
+        }
+
+        DbgPrint("ntoskrnl.exe at 0x%p\n", NtImageBase);
+        return NtImageBase;
+    }
+    __except (1) {}
+}
+
+/*
+PVOID GetNtOsKrnlWithMsr(VOID)
+{
+    auto page = PAGE_ALIGN(__readmsr(IA32_LSTAR));// &~(PAGE_SIZE - 1); // PC page
+
+    do {
+        auto addr = *(USHORT *)(page);
+        if (addr == IMAGE_DOS_SIGNATURE) // M Z header
+        {
+            for (auto i = page; i < page + PTE_ENTRY_COUNT_32; i += 8) {
+                if (*reinterpret_cast<ULONG64*>(i) == PAGELK) // find PAGELK section
+                    return reinterpret_cast<PVOID>(page);
+            }
+        }
+        page -= PAGE_SIZE;
+    } while (true);
+
+    return nullptr;
+}
+*/
+
 void Unload(PDRIVER_OBJECT)
 {
     DbgPrint("Driver unloaded\n");
@@ -147,8 +219,14 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, PUNICODE_STRING)
 {
     DbgPrint("Driver loaded\n");
 
-    //ListModulesWithAuxKlib();
-    ListModulesWithQuerySystemInformation();
+    DbgPrint("* GetNtOsKrnlWithAuxLib\n");
+    GetNtOsKrnlWithAuxKlib();
+    DbgPrint("* GetNtOsKrnlWithQuerySystemInformation\n");
+    GetNtOsKrnlWithQuerySystemInformation();
+    DbgPrint("* GetNtOsKrnlWithPC\n");
+    GetNtOsKrnlWithPC();
+    DbgPrint("* GetNtOsKrnlWithPC2\n");
+    GetNtOsKrnlWithPC2();
 
     DriverObject->DriverUnload = Unload;
     return STATUS_SUCCESS;
